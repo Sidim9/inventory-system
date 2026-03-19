d0e4af636743c100e5f15ece9c378fe024fef225
Sidim9 <124693825+Sidim9@users.noreply.github.com>
2026-03-19 16:08:40 +0100

    Register web routes for inventory system modules

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,18 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\MarketplaceController;
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\OrderItemController;
-use App\Http\Controllers\OrderAddressController;
 use App\Http\Controllers\StockMovementController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+
 Route::resource('products', ProductController::class);
-Route::resource('marketplaces', MarketplaceController::class);
 Route::resource('orders', OrderController::class);
 Route::resource('order_items', OrderItemController::class);
-Route::resource('order_addresses', OrderAddressController::class);
 Route::resource('stock_movements', StockMovementController::class);
+Route::resource('marketplaces', MarketplaceController::class);
+
